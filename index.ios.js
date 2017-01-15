e65194442269ec70bc9f0c42afa694506ef4202a
gchasep <gchase.patron@gmail.com>
2017-01-15 20:16:15 +0800

    Header done

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+
+import Header from './src/components/header';
 
 const App = () => {
   return (
-    <Text>Hello React Native!</Text>
+    <Header headerText={'Albums!'} />
   );
 };
 
